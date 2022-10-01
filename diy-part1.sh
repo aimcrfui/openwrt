fea8b77d8c87e328969002d4da6c8d2142d803a1
aimcrfui <43334837+aimcrfui@users.noreply.github.com>
2022-10-01 18:06:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,101 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加源
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 修改openwrt登陆地址,把下面的192.168.2.22修改成你想要的就可以了
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把Unicorn修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='Ccoo'" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/hostname='OpenWrt'/hostname='Ccoo'/g" ./package/base-files/files/bin/config_generate
+
+# 晶晨宝盒
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+sed -i "s|https://github.com/breakings/OpenWrt|https://github.com/MXJNZ6/Flippy-D|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|s905d|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|opt/kernel|kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|.img.gz|待定|g" package/luci-app-amlogic/root/etc/config/amlogic
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+#svn co https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash package/luci-app-openclash
+#svn co https://github.com/linkease/nas-packages/trunk/network/services/ddnsto package/ddnsto
+#svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto package/luci-app-ddnsto
+svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/luci-app-aliyundrive-webdav
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+# luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+sed -i 's/system/services/g'  package/luci-app-argon-config/luasrc/controller/argon-config.lua
+
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/honwen/luci-app-xray.git package/luci-app-xray
+
+# Update feeds
+./scripts/feeds update -a
+
+# 删除重复包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/smartdns
+#rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+#rm -rf feeds/luci/applications/luci-app-dockerman
+rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
+rm -rf feeds/luci/applications/luci-app-unblockmusic
+rm -rf feeds/packages/multimedia/UnblockNeteaseMusic-Go
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+
+# Install feeds
+./scripts/feeds install -a
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd package/luci-app-xray/tools/po2lmo
+make && sudo make install
+popd
+
+# 调整VPN服务到VPN菜单
+# v2ray服务
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
+# wireguard
+sed -i 's/status/vpn/g' feeds/luci/applications/luci-app-wireguard/luasrc/controller/wireguard.lua
+sed -i 's/92/2/g' feeds/luci/applications/luci-app-wireguard/luasrc/controller/wireguard.lua
+# frps
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-frps/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-frps/luasrc/model/cbi/frps/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-frps/luasrc/view/frps/*.htm
+# frpc
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-frpc/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-frpc/luasrc/model/cbi/frp/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-frpc/luasrc/view/frp/*.htm
+# 花生壳内网穿透
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-phtunnel/luasrc/controller/oray/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-phtunnel/luasrc/model/cbi/oray/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-phtunnel/luasrc/view/oray/*.htm
+# 蒲公英组网
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-pgyvpn/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-pgyvpn/luasrc/model/cbi/*.lua
+sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-pgyvpn/luasrc/view/pgyvpn/*.htm
+
+# 调整阿里云盘到存储菜单
+sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/controller/*.lua
+sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/model/cbi/aliyundrive-webdav/*.lua
+sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/*.htm
+
+# 修改插件名字
+sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/po/zh-cn/aliyundrive-webdav.po
+sed -i 's/WireGuard 状态/WireGuard/g' feeds/luci/applications/luci-app-wireguard/po/zh-cn/wireguard.po
